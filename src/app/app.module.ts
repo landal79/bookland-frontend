5484b763dc9274a383498217159c24cc0beac09c
landal79 <alex.landini@gmail.com>
2017-01-11 00:12:39 +0100

    moved editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import { CKEditorModule } from 'ng2-ckeditor';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
 
-import { AppComponent } from './app.component';
-import { routing, routedComponents } from './app.routing';
-import { BookListComponent } from './book-list/book-list.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { BookEditComponent } from './book-edit/book-edit.component';
-import { BookService } from "./book.service";
-import { EditorComponent } from './editor/editor.component';
-import { _404Component } from './404-component/404';
+import {AppComponent} from './app.component';
+import {routing, routedComponents} from './app.routing';
+import {BookListComponent} from './book-list/book-list.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
+import {BookEditComponent} from './book-edit/book-edit.component';
+import {BookService} from "./book.service";
+import {_404Component} from './404-component/404';
 import {ComponentsModule} from "./components/components.module";
 
 @NgModule({
@@ -21,7 +19,6 @@ import {ComponentsModule} from "./components/components.module";
     BookListComponent,
     BookDetailComponent,
     BookEditComponent,
-    EditorComponent,
     _404Component
   ],
   imports: [
@@ -30,10 +27,10 @@ import {ComponentsModule} from "./components/components.module";
     routing,
     HttpModule,
     ComponentsModule,
-    CKEditorModule,
     MaterialModule.forRoot()
   ],
   providers: [BookService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StarsComponent } from './stars/stars.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StarsComponent} from './stars/stars.component';
+import {EditorComponent} from "./editor/editor.component";
+import {CKEditorModule} from "ng2-ckeditor";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    CKEditorModule
   ],
-  declarations: [StarsComponent],
-  exports :[StarsComponent]
+  declarations: [StarsComponent, EditorComponent],
+  exports: [StarsComponent, EditorComponent]
 })
-export class ComponentsModule { }
+export class ComponentsModule {
+}
